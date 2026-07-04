1447ff98b17c5ee42887fbb0801416a725eb8543
Mohammad alalaq <61648474+Mohammad657@users.noreply.github.com>
2026-07-04 15:54:38 +0300

    

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,7 +27,7 @@ class HomePage extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: const Text(
-              "Point Counter",
+              "Point counter",
               textAlign: TextAlign.end,
             ),
             backgroundColor: Colors.orange,
